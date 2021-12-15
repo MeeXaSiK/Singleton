d2eafb44bb218490445aa31fb005d5a09a58b9c4
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2021-12-15 13:54:43 +0700

    Singleton

--- System/Singleton.cs
+++ System/Singleton.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿// ----------------------------------------------------------------------------
+// The MIT License
+// Singleton for Unity https://github.com/MeeXaSiK/NightSingleton
+// Copyright (c) 2021 Night Train Code
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
 
 namespace NTC.Global.System
 {
