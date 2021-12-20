e0c57830dcc113701160a9fbba3d0113efb4c648
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2021-12-21 00:34:34 +0700

    Update Singleton.cs

--- Code/System/Singleton.cs
+++ Code/System/Singleton.cs
@@ -1,8 +1,8 @@
-﻿// ----------------------------------------------------------------------------
+﻿// ----------------------------------------------------------------
 // The MIT License
 // Singleton for Unity https://github.com/MeeXaSiK/NightSingleton
 // Copyright (c) 2021 Night Train Code
-// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------
 
 using UnityEngine;
 
@@ -16,21 +16,39 @@ namespace NTC.Global.System
             {
                 if (cachedInstance == null)
                 {
-                    var instances = FindObjectsOfType<TSingleton>();
-                    var instance = instances.Length > 0 
-                        ? instances[0] 
-                        : new GameObject($"[Singleton] {typeof(TSingleton).Name}").AddComponent<TSingleton>();
-
-                    for (var i = 1; i < instances.Length; i++)
-                    {
-                        Destroy(instances[i]);
-                    }
-
-                    return cachedInstance = instance;
+                    return cachedInstance = GetNotNull();
                 }
 
                 return cachedInstance;
             }
+        }
+
+        public static TSingleton GetCanBeNull()
+        {
+            var instances = FindObjectsOfType<TSingleton>();
+            var instance = instances.Length > 0 ? instances[0] : null;
+
+            for (var i = 1; i < instances.Length; i++)
+            {
+                Destroy(instances[i]);
+            }
+
+            return instance;
+        }
+
+        private static TSingleton GetNotNull()
+        {
+            var instances = FindObjectsOfType<TSingleton>();
+            var instance = instances.Length > 0 
+                ? instances[0] 
+                : new GameObject($"[Singleton] {typeof(TSingleton).Name}").AddComponent<TSingleton>();
+
+            for (var i = 1; i < instances.Length; i++)
+            {
+                Destroy(instances[i]);
+            }
+
+            return instance;
         }
         
         private static TSingleton cachedInstance;
